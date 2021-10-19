98bcb21e5a132656f3ae6f2fcf4d5e0e4b863c0d
Katayama <katayama.takuma@jp.fujitsu.com>
2021-10-19 22:47:00 +0900

    #2 HTMLをファイルから読み込むように修正

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const http = require('http');
+var fs = require('fs');
 
-http.createServer(function (req, res, ) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end(`Hello World!! \n`)
-}).listen('8080');
+var server = http.createServer(function (req, res) {
+  var url = "public" + (req.url.endsWith("/") ? req.url + "index.html" : req.url);
+  if (fs.existsSync(url)) {
+    fs.readFile(url, (err, data) => {
+      if (!err) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      } else {
+        res.statusCode = 500;
+        res.end();
+      }
+    });
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+});
+
+var port = process.env.PORT || 8080;
+server.listen(port, function () {
+  console.log("Server Start. port:" + port);
+});
